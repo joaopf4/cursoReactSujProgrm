a590f73ba7b4ecad256063889662d1cb613e13ff
Joao Pedro <joaopfa@hotmail.com>
2021-03-26 11:58:41 -0300

    aula 46 cronometro

--- src/Aula46.js
+++ src/Aula46.js
@@ -5,7 +5,8 @@ class Aula46 extends Component{
     constructor(props){
         super(props);
         this.state = {
-            numero: 0
+            numero: 0,
+            botao: 'VAI'
         };
         this.timer = null;
         this.vai = this.vai.bind(this);
@@ -13,14 +14,31 @@ class Aula46 extends Component{
     }
 
     vai(){
-        this.timer = setInterval(() => {
-            let state = this.state;
-            state.numero += 0.1;
-            this.setState(state);
-        }, 100)
+        let state = this.state;
+        if(this.timer !== null){
+            clearInterval(this.timer);
+            this.timer = null;
+            state.botao = 'VAI';
+        } else {
+            this.timer = setInterval(() => {
+                let state = this.state;
+                state.numero += 0.01;
+                this.setState(state);
+            }, 10)
+            state.botao = 'PAUSAR';
+        }
+        this.setState(state);
     }
 
     limpar(){
+        if(this.timer !== null){
+            clearInterval(this.timer);
+            this.timer = null;
+        }
+        let state = this.state;
+        state.numero = 0;
+        state.botao = 'VAI';
+        this.setState(state);
 
     }
 
@@ -28,16 +46,16 @@ class Aula46 extends Component{
         return(
             <div className="container">
                 <img alt="cronometro" src={Cronometro} className="img"/>
-                <a className="timer">{this.state.numero.toFixed(1)}</a>
+                <p className="timer">{this.state.numero.toFixed(2)}</p>
                 <div className="areaBtn">
-                    <a 
-                    className="botao"
-                    onClick={this.vai}
-                    >Vai</a>
-                    <a 
-                    className="botao"
-                    onClick={this.limpar}
-                    >Limpar</a>
+                    <button
+                        className="botao"
+                        onClick={this.vai}
+                    >{this.state.botao}</button>
+                    <button
+                        className="botao"
+                        onClick={this.limpar}
+                    >Limpar</button>
                 </div>
             </div>
         );
